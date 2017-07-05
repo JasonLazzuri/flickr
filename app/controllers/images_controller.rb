fc0c7a3b661fc51058976cf2e14cc2d87dd14fe7
Dan Jacob and Jason Lazzuri <dan+jason@>
2017-07-05 13:35:09 -0700

    setup

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -9,7 +9,16 @@ class ImagesController < ApplicationController
     @images = Image.all
   end
 
+  def create
+    @image = Image.create(image_params)
+  end
+
   def new
     @image = Image.new
   end
+end
+
+private
+  def image_params
+    params.require(:image).permit(:description,:image)
 end
